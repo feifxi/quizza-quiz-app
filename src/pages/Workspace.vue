7669f752fcba51a6d699ec84f24b511f9df025fa
IjustAguy <pakin.toom@kmutt.ac.th>
2025-03-13 22:34:55 +0700

    071[Update]update workspace page

--- src/pages/Workspace.vue
+++ src/pages/Workspace.vue
@@ -1,41 +1,44 @@
 <script setup>
 import { getAllQuizs, getQuizById, deleteQuiz } from '@/api/quizsAPI';
-import { onBeforeMount, onUpdated, ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { onBeforeMount, ref } from 'vue';
 
-const router = useRouter();
-
+// quizz of admin and user
 const userQuizzes = ref([]);
 const adminQuizzes = ref([])
 
+let CheckUser = localStorage.getItem('user');
+const userObj = JSON.parse(CheckUser);
 
-let Check = localStorage.getItem('user');
-let UsedUser 
-let UsedRole
+let UsedRole = userObj.role
+let UsedUser = ref(userObj.id)
+let testUser = userObj.userName
 
+// check Username and Role
+console.log("user : " + testUser + " role : " + UsedRole)
 
-if (Check) {
-  const userObj = JSON.parse(Check);
-  UsedUser = userObj.userName;
-  UsedRole = userObj.role;
-  console.log(UsedRole)  
-  console.log(UsedUser);
-} else {
-  alert('ยังไม่ได้Login กรุณาloginก่อน');
-  router.push("/")
+// chage State By AdminRole
+let state = ref(true);
+const headAdmin = (data) => {
+  if(data === 'admin'){
+    state.value = false
+
+  }
+  if(data === 'workspace'){
+    state.value = true
+  }
 }
+
 
 // GET
 onBeforeMount(async () => {
   try {
     const res = await getAllQuizs();
-    const adminData = await res.data;
-    const userData = await res.data;
+    const adminData = res.data; 
+    const userData = res.data;
     adminQuizzes.value = adminData;
-    userQuizzes.value = userData;
-    console.log(userQuizzes)
+    userQuizzes.value = userData.filter(quiz => quiz.createBy.id === UsedUser.value)
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 });
 
@@ -46,7 +49,6 @@ const deleteQuizById = async (quizId) => {
     const data = await res.data;
     if (data) {
       await deleteQuiz(quizId);
-      const removeIndex = quizzes.value.findIndex((quiz) => quiz.id = quizId);
       quizzes.value.splice(removeId, 1);
       isSuccess.value = true;
     } else {
@@ -57,11 +59,21 @@ const deleteQuizById = async (quizId) => {
   }
 }
 
+
+
+
 </script>
 
 <template>
+  <!-- head for Admin -->
+   <section v-if="UsedRole === 'admin'">
+    <div class="flex flex-row">
+      <button class="border-1 border-red-500 p-2 m-2" @click="headAdmin('workspace')">WorkSpace</button>
+      <button class="border-1 border-red-500 p-2 m-2" @click="headAdmin('admin')">AdminReview</button>
+    </div>
+   </section>
     <!-- Addmin Review -->  
-<section v-if="UsedRole === 'admin'">
+<section v-if="!state && UsedRole === 'admin'">
   <h1>Game Review</h1>
     <div class="m-1">
       <ul>
@@ -98,7 +110,7 @@ const deleteQuizById = async (quizId) => {
     </div>
 </section>
   <!-- user workspace -->
-  <section v-else-if="UsedRole === 'user'">
+  <section v-else-if="state">
     <h1>Workspace</h1>
     <div class="m-1">
       <ul>
@@ -110,7 +122,12 @@ const deleteQuizById = async (quizId) => {
           cursor-pointer hover:text-gray-800 hover:bg-amber-500">
             Edit
           </button>
-          
+          <button class="mt-1 px-1 absolute right-3 border border-black text-black bg-red-600
+          cursor-pointer hover:text-gray-800 hover:bg-red-500"
+          @click="deleteQuizById(quiz.id)">
+            X
+          </button>
+
           <p>
             <span><img :src="quiz.thumbnail" alt="vue logo" class="max-w-40" /></span>
             <span>ID: </span>{{ quiz.id }}<br />
@@ -122,16 +139,10 @@ const deleteQuizById = async (quizId) => {
           cursor-pointer hover:text-gray-200 hover:bg-blue-500">
             Comment
           </button>
-          <button class="px-1 absolute right-3 border border-black text-black bg-green-600
-          cursor-pointer hover:text-gray-800 hover:bg-green-500">
-            Approve
-          </button>
         </li>
       </ul>
     </div>
   </section>
-
 </template>
-
 <style scoped>
 </style>
