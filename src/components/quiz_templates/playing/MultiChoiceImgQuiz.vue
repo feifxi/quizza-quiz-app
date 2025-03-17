be90664c5c248536090ed24e1a65860a9d9d13ca
hello <feoy1234@gmail.com>
2025-03-17 21:17:54 +0700

    125 - [update] update playing quiz page

---
+++ src/components/quiz_templates/playing/MultiChoiceImgQuiz.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const { levelData } = defineProps({
+    levelData: Object,
+  });
+
+
+</script>
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/quiz_templates/playing/MultiChoiceTextQuiz.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Button from '@/components/Button.vue';
+  const { levelData, goNext, increaseScore } = defineProps({
+    levelData: Object,
+    goNext: Function, 
+    increaseScore: Function,
+  });
+
+  console.log(levelData)
+</script>
+<template>
+  <div class="border border-black p-3">
+    <h2 class="">Multiple Choice Text Template</h2>
+    <Button :label="'go next'" :click="goNext"></Button>
+    <Button :label="'increaseScore'" :click="increaseScore"></Button>
+  </div>
+</template>
+
+<style scoped></style>
