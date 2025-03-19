44151944bb9720b56c6ddc8b36a88f95f384519a
noppawut phomchana <142074952+saigrogyh@users.noreply.github.com>
2025-03-19 19:57:06 +0700

    114[update]update user star

--- src/pages/LeaderBoard.vue
+++ src/pages/LeaderBoard.vue
@@ -1,26 +1,46 @@
 <script setup>
 import { computed, onBeforeMount, reactive, ref } from "vue";
+import { getAllQuizs } from "@/api/quizsAPI";
+import { getAllUsers } from "@/api/usersAPI";
 
-let listUsersData = ref([]);
-async function listByMostLike() {
-  try {
-    const responseData = await fetch("http://localhost:5000/users");
-    listUsersData.value = await responseData.json();
-  } catch (error) {
-    console.error("Error muhaha:", error);
-  }
-}
+let listQuizData = ref([]);
+let listUser = ref([]);
+let loading = ref(false);
 
-const sortUsersByLike = computed(() =>
-  listUsersData.value.slice().sort((a, b) => b.star - a.star)
+const fetchAllQuizsAndUsers = async () => {
+  loading.value = true;
+  const resQ = await getAllQuizs([{ key: "status", value: "publish" }]);
+  const resU = await getAllUsers();
+  listQuizData.value = resQ.data;
+  listUser.value = resU.data;
+  loading.value = false;
+};
+
+const sortUsersByStar = computed(() =>
+  listUser.value.slice().sort((a, b) => b.star - a.star)
 );
 
-onBeforeMount(() => {
-  listByMostLike();
+onBeforeMount(async () => {
+  await fetchAllQuizsAndUsers();
+
+  let prog = listQuizData.value.flatMap((quiz) => quiz.playerProgress || []);
+  console.log("prog : ", prog);
+
+  const userStar = new Map();
+  prog.forEach(({ userId, star }) => {
+    const getStar = userStar.get(userId) || 0;
+    userStar.set(userId, getStar + star);
+  });
+
+  listUser.value = listUser.value.map((user) => ({
+    ...user,
+    star: userStar.get(user.id) || 0,
+  }));
 });
 </script>
 
 <template>
+  <div v-if="loading">Loading...</div>
   <section>
     <h1>Leader Board</h1>
     <table>
@@ -33,7 +53,7 @@ onBeforeMount(() => {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(user, index) in sortUsersByLike" :key="user.id">
+        <tr v-for="(user, index) in sortUsersByStar" :key="user.id">
           <td>{{ index + 1 }}</td>
           <td>{{ user.id }}</td>
           <td>{{ user.userName }}</td>
